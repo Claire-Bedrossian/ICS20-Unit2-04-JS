7928ebf089697c5a4c7f02143db47b4b68ee4c7b
Claire-Bedrossi <113055648+Claire-Bedrossian@users.noreply.github.com>
2022-10-16 16:00:35 +0000

    Added Functions

---
+++ js/script.js
@@ -0,0 +1,27 @@
+// Copyright (c) 2022 Claire Bedrossian All rights reserved
+//
+// Created by: Claire Bedrossian
+// Created on: Oct 2022
+// This file contains the JS functions for index.html
+
+'use strict'
+/**
+ * This function calculates the area and perimeter of a triangle with user input
+ */
+function myButtonClicked() {
+  // input
+  const base = parseInt(document.getElementById("base-number").value
+  const length = parseInt(document.getElementById("length-number").value)
+  const sideA = parseInt(document.getElementById("side-a-number").value)
+  const sideC = parseInt(document.getElementById("side-c-number").value)
+
+  //process
+
+  const area = ( base * height ) /2
+  const perimeter = sideA + sideC + base
+
+  // output
+  document.getElementById('area').innerHTML = 'Area is: ' + area + ' cm²'
+  document.getElementById('perimeter').innerHTML = 'Perimeter is: ' + perimeter + ' cm'
+}
+  
